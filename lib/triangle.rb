5a15c7331d30ba49e157b83d38ff35618ba2ef4e
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-04-08 00:55:28 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def validate_triangle #couldn't get this to work, took this from Chris
+    real_triangle = [(side1 + side2 > side3),
+      (side1 + side3 > side2),
+      (side2 + side3 > side1)]
+    [side1, side2, side3].each do |s|
+      real_triangle << false if s <= 0
+    end
+    raise TriangleError if real_triangle.include?(false)
+  end
+
+
+  def kind
+    validate_triangle
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
